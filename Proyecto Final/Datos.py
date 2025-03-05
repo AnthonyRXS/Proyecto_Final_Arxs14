36a0eae6acf833f623050449bd1cb230613d8e9a
AnthonyRXS <arxs74@gmail.com>
2025-03-04 18:53:07 -0600

    actualizar

---
+++ Proyecto Final/Datos.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Cargar el archivo CSV
+df = pd.read_csv('Carbon_Dioxide_Emission_Estimates.csv')
+
+
+# Filtrar los datos según la columna 'Type'
+df_emisions_co2 = df[df['Type'] == 'Emissions (thousand metric tons of carbon dioxide)']
+df_emisions_co2_per_capita = df[df['Type'] == 'Emissions per capita (metric tons of carbon dioxide)']
+
+# Mostrar los primeros registros de cada DataFrame
+print(df_emisions_co2.head())
+print(df_emisions_co2_per_capita.head())
+
+
